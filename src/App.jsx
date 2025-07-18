aa386d8de2e4039985610029a05371fd827e4d3c
Damian <dd.laskowski@gmail.com>
2025-07-18 20:22:59 +0200

    Initial commit with project files

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {Home, About, Contact, Projects} from './pages';
+
+import './index.css';
+import Navbar from './components/Navbar';
+
+
+
+const App = () => {
+    return ( 
+        <main className='bg-slate-300/20'>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path='/' element={<Home/>} />
+                    <Route path='/about' element={<About/>} />
+                    <Route path='/projects' element={<Projects/>} />
+                    <Route path='/contact' element={<Projects/>} />
+                </Routes>
+            </Router>
+        </main>
+
+     );
+}
+ 
+export default App;
